3b1139f8b2e58d24c9a4ddaeaaae077dd1013c36
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-29 11:42:38 -0700

    add more space complexity explanation

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -18,4 +18,4 @@ end
 
 
 # the time complexity for this solution is O(n * m) because in a worst case scenario is will iterate through each element in m for each element in n
-# the space complexity is O(n) where n is smaller than m because the new array created to contain the intersecting elements cannot excede the number of elements in the smaller array
+# the space complexity is O(n) where n is smaller than m because the new array created to contain the intersecting elements cannot excede the number of elements in the smaller array. An alternative solution with higher space complexity would involve creating a hash and copying each element from one of the arrays over to the hash, where each element represents one key in the hash with the corresponding value being nil. You could then iterate through each element in the second array, attempting to look it up in the hash. If there is an intersection, you will receive a truthy reponse; if no intersection, the lookup will return false.
